f6880eca83e03fcac2fe96630f6faec7b4e39efa
Jonas Siewertsen <jonas.siewertsen@spiegel.de>
2022-06-01 17:54:59 +0200

    Register the addon the tiptap2 way

--- resources/js/bard.js
+++ resources/js/bard.js
@@ -1,16 +1,17 @@
 import ArckFontWeight from './ArckFontWeight';
 import ArckFontWeightMenu from './ArckFontWeightMenu.vue';
 
-Statamic.$bard.extend(({mark}) => mark(new ArckFontWeight()));
+Statamic.$bard.addExtension(() => ArckFontWeight);
+
+Statamic.$components.register('ArckFontWeightMenu', require('./ArckFontWeightMenu.vue').default);
+
 Statamic.$bard.buttons(() => {
     return {
-        name: 'arckfontweight',
+        name: 'arckFontWeight',
         text: 'Font Weight',
-        command: 'textStyle',
-        args: {
-            key: ""
-        },
+        args: { key: "" },
         icon: 'arck-font-weight',
-        component: ArckFontWeightMenu
+        component: ArckFontWeightMenu,
+        command: (editor) => editor.command.setCustomExtension(),
     };
 });
